2a9c714b119ff06e9665059d1adee3c3d10fb3c3
Nelson Seixas <nsouzarj@outlook.com>
2026-04-05 11:01:54 -0300

    fix(seguranca)

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://iwtdipkqjyibbkppjvcl.supabase.co';
-const supabaseKey = 'sb_publishable_w5CCa0L1aN3a91jVPHoXtg_nw1LQdB-';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
